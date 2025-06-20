e2ef83dd3c628e294768a36386a51b0444c3fbfe
froster441 <froster441@gmail.com>
2025-06-20 08:42:48 +0530

    chore(auth-service): removed status code in not found error response,changed status code.

--- services/auth-service/src/infrastructure/http/middleware/not-found-handler.middleware.ts
+++ services/auth-service/src/infrastructure/http/middleware/not-found-handler.middleware.ts
@@ -2,9 +2,8 @@ import { Context } from 'hono';
 import httpStatus from 'http-status';
 
 export function notFoundHandler(c: Context) {
-  c.status(httpStatus.NOT_EXTENDED);
+  c.status(httpStatus.NOT_FOUND);
   return c.json({
-    statusCode: httpStatus.NOT_FOUND,
     message: httpStatus[httpStatus.NOT_FOUND],
     path: c.req.url,
   });
