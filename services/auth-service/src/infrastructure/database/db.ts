1af3e57950c79e568df8ef84f30d44a3df7ad585
froster441 <froster441@gmail.com>
2025-06-20 08:35:16 +0530

    refactor(auth-service): added schema to database instance to get proper typing.

--- services/auth-service/src/infrastructure/database/db.ts
+++ services/auth-service/src/infrastructure/database/db.ts
@@ -1,9 +1,10 @@
 import { drizzle } from 'drizzle-orm/node-postgres';
 import { databaseConfig } from '@/shared/config/database.config';
 import { Pool } from 'pg';
+import * as schema from './schema';
 
 const pool = new Pool({
   connectionString: databaseConfig.DATABASE_URL,
 });
 
-export const db = drizzle(pool);
+export const db = drizzle({ client: pool, schema });
