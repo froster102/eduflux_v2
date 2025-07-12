d367d21582dc9b26c1f314074df6dedf1db016bc
froster441 <froster441@gmail.com>
2025-07-12 15:22:24 +0530

    refactor(enrollment-service): create enrollment schema validation change.

--- services/enrollment-service/src/infrastructure/http/schema/enrollment.schema.ts
+++ services/enrollment-service/src/infrastructure/http/schema/enrollment.schema.ts
@@ -1,5 +1,5 @@
 import z from 'zod/v4';
 
 export const createEnrollmentSchema = z.object({
-  courseId: z.uuidv4({ error: 'Invalid course ID' }),
+  courseId: z.string({ error: 'Invalid course ID' }),
 });
