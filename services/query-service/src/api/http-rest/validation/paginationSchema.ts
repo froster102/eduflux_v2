289269656d61d61329b32d9ca9524ca8ae967fff
froster441 <froster441@gmail.com>
2025-10-01 18:41:48 +0530

    refactor(query-service): added number coerce to pagination validation schema.

--- services/query-service/src/api/http-rest/validation/paginationSchema.ts
+++ services/query-service/src/api/http-rest/validation/paginationSchema.ts
@@ -2,6 +2,6 @@ import { PaginationConfig } from "@shared/config/PaginationConfig";
 import { z } from "zod/v4";
 
 export const paginationSchema = z.object({
-  page: z.number().default(1),
-  limit: z.number().default(PaginationConfig.defaultPageSize),
+  page: z.coerce.number().default(1),
+  limit: z.coerce.number().default(PaginationConfig.defaultPageSize),
 });
