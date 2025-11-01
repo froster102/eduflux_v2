9bd424114416bdd27b9a77d4330e53230aa09be3
froster441 <froster441@gmail.com>
2025-11-01 16:35:32 +0530

    refactor(user-service): refactored user service to used the shared winston logger adapter.

--- apps/user-service/src/di/InfrastructureModule.ts
+++ apps/user-service/src/di/InfrastructureModule.ts
@@ -2,7 +2,6 @@ import { SharedCoreDITokens } from '@eduflux-v2/shared/di/SharedCoreDITokens';
 import type { MessageBrokerPort } from '@eduflux-v2/shared/src/ports/message/MessageBrokerPort';
 import type { LoggerPort } from '@eduflux-v2/shared/ports/logger/LoggerPort';
 import { InfrastructureDITokens } from '@infrastructure/di/InfrastructureDITokens';
-import { WinstonLogger } from '@infrastructure/logging/WinstonLoggerAdapter';
 import { ContainerModule } from 'inversify';
 import { RabbitMQConfig } from '@shared/config/RabbitMQConfig';
 import { RabbitMqConnection } from '@eduflux-v2/shared/infrastructure/messaging/rabbitmq/RabbitMQConnection';
@@ -13,11 +12,24 @@ import { RabbitMQqueueFormatter } from '@eduflux-v2/shared/infrastructure/messag
 import { MongooseConnection } from '@eduflux-v2/shared/infrastructure/database/mongoose/MongooseConnection';
 import { GrpcUserServiceController } from '@api/grpc/controller/GrpcUserServiceController';
 import { MongooseConnectionConfig } from '@infrastructure/config/MongooseConnectionConfig';
+import { envVariables } from '@shared/env/env-variables';
+import { asyncLocalStorage } from '@shared/utils/async-store';
+import { USER_SERVICE } from '@shared/constants/services';
+import { WinstonLoggerAdapter } from '@eduflux-v2/shared/adapters/logger/WinstonLoggerAdapter';
+import type { LoggerConfig } from '@eduflux-v2/shared/config/LoggerConfig';
 
 export const InfrastructureModule: ContainerModule = new ContainerModule(
   (options) => {
     //Logger
-    options.bind<LoggerPort>(SharedCoreDITokens.Logger).to(WinstonLogger);
+    const config: LoggerConfig = {
+      environment: envVariables.NODE_ENV,
+      serviceName: USER_SERVICE,
+      asyncLocalStorage: asyncLocalStorage,
+      enableCorrelationId: true,
+    };
+    options
+      .bind<LoggerPort>(SharedCoreDITokens.Logger)
+      .toConstantValue(new WinstonLoggerAdapter(config));
 
     //Controller
     options
