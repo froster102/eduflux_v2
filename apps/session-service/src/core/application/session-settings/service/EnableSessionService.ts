a702a98d5407644ec9664d2a83a5e7348396edc0
froster441 <froster441@gmail.com>
2025-11-03 18:20:05 +0530

    refactor(session-service): added authorization before enabling sessions.

--- apps/session-service/src/core/application/session-settings/service/EnableSessionService.ts
+++ apps/session-service/src/core/application/session-settings/service/EnableSessionService.ts
@@ -5,17 +5,18 @@ import type { UnitOfWork } from '@core/common/port/persistence/UnitOfWorkPort';
 import { SlotGenerationService } from '@core/domain/service/SlotGenerationService';
 import { SessionSettings } from '@core/domain/session-settings/entity/SessionSettings';
 import { SessionSettingsUpdateEvent } from '@eduflux-v2/shared/events/session/SessionSettingsUpdateEvent';
-import { SessionSettingsEvents } from '@eduflux-v2/shared/events/session/enum/SessionSettingsEvents';
 import { inject } from 'inversify';
 import { v4 as uuidV4 } from 'uuid';
 import type { UserServicePort } from '@eduflux-v2/shared/ports/gateway/UserServicePort';
 import { SharedCoreDITokens } from '@eduflux-v2/shared/di/SharedCoreDITokens';
 import type { MessageBrokerPort } from '@eduflux-v2/shared/ports/message/MessageBrokerPort';
+import { BadRequestException } from '@eduflux-v2/shared/exceptions/BadRequestException';
+import { Role } from '@eduflux-v2/shared/constants/Role';
 
 export class EnableSessionService implements EnableSessionsUseCase {
   constructor(
     @inject(SharedCoreDITokens.UserService)
-    private readonly userServiceGateway: UserServicePort,
+    private readonly userService: UserServicePort,
     @inject(SharedCoreDITokens.UnitOfWork)
     private readonly uow: UnitOfWork,
     @inject(SharedCoreDITokens.MessageBroker)
@@ -26,10 +27,16 @@ export class EnableSessionService implements EnableSessionsUseCase {
     const { executorId, applyForWeeks, price, timeZone, weeklySchedules } =
       payload;
 
-    const user = await this.userServiceGateway.getUser(executorId);
+    const user = await this.userService.getUser(executorId);
 
     if (!user) {
       throw new NotFoundException(`User with ID:${executorId} not found.`);
+    }
+
+    if (!user.roles.includes(Role.INSTRUCTOR)) {
+      throw new BadRequestException(
+        'User is not an instructor.Please become an instructor first.',
+      );
     }
 
     const sessionSettings = SessionSettings.create({
