e3184a283eadd3918f2f6eefa5967d30eaff8566
froster441 <froster441@gmail.com>
2025-11-07 20:41:35 +0530

    refactor(chat-service): refactored user chat created event to find the event class structure.

--- apps/chat-service/src/core/application/views/user-chat/events/UserChatCreatedEvent.ts
+++ apps/chat-service/src/core/application/views/user-chat/events/UserChatCreatedEvent.ts
@@ -2,7 +2,7 @@ import { ChatEvents } from '@core/application/views/user-chat/events/enum/ChatEv
 import { Event } from '@eduflux-v2/shared/events/Event';
 import type { ChatParticipant } from '@core/domain/chat/entity/Chat';
 
-export interface UserChatCreatedEventPayload extends Event {
+export interface UserChatCreatedEventPayload {
   readonly participants: ChatParticipant[];
   readonly lastMessageAt: string;
   readonly createdAt: string;
@@ -12,10 +12,10 @@ export interface UserChatCreatedEventPayload extends Event {
 export class UserChatCreatedEvent extends Event<UserChatCreatedEventPayload> {
   static readonly EVENT_NAME = ChatEvents.USER_CHAT_CREATED;
 
-  constructor(payload: UserChatCreatedEventPayload) {
+  constructor(id: string, payload: UserChatCreatedEventPayload) {
     super(
       {
-        id: payload.id,
+        id,
         name: ChatEvents.USER_CHAT_CREATED,
       },
       payload,
